d6ddb988d0c409ae173d5eb045a9e76f66182651
nwisemanII <nwiseman@bluebastion.net>
2024-05-23 09:57:29 -0400

    fix unix section

--- bb-lib-http-listener/src/shutdown.rs
+++ bb-lib-http-listener/src/shutdown.rs
@@ -7,7 +7,7 @@ pub async fn shutdown_signal() {
 
     #[cfg(unix)]
     let terminate = async {
-        tokio::signal::unix::signal(signal::unix::SignalKind::terminate())
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
             .expect("failed to install signal handler")
             .recv()
             .await;
