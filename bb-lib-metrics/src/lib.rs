56dc981e3641f4505e09d125c3cde64fb60b19f5
nwisemanII <nwiseman@bluebastion.net>
2024-07-18 14:09:13 -0400

    added build_metrics

--- bb-lib-metrics/src/lib.rs
+++ bb-lib-metrics/src/lib.rs
@@ -1,20 +1,8 @@
-use std::future::Future;
-use std::pin::Pin;
-use std::{net::SocketAddr, time::Duration};
-
+use metrics_exporter_prometheus::PrometheusBuilder;
+use std::net::SocketAddr;
 use anyhow::Error;
-use metrics::counter;
-use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusRecorder};
 
-pub async fn init_metrics() -> Result<
-    (),
-    // //  Return Recoreder/Future
-    // (
-    //     PrometheusRecorder,
-    //     Pin<Box<dyn Future<Output = Result<(), hyper::Error>> + Send>>,
-    // ),
-    Error,
-> {
+pub async fn init_metrics() -> Result<(), Error> {
     PrometheusBuilder::new()
         // // For Push Gateway
         // .with_push_gateway(
@@ -24,18 +12,16 @@ pub async fn init_metrics() -> Result<
         .with_http_listener(SocketAddr::new(
             "0.0.0.0".parse().expect("Couldn't Parse IP"),
             9010,
-        )).install()?;
-    
-    // //  Return Recoreder/Future
-    // let builder = PrometheusBuilder::new();
-    // builder.install().expect("Failed to install recorder/exporter");
-    // let (recorder, exporter) = builder.build().expect("Failed to build Recorder/Exporter");
-    // Ok((recorder, exporter))
+        ))
+        .install()?;
     Ok(())
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
+    use super::*;
+    use metrics::counter;
+
     fn add_counter() -> String {
         let counter = counter!("counter.nicotest", "service" => "http");
         counter.increment(1);
@@ -62,5 +48,4 @@ mod tests{
         }
         Ok(())
     }
-
 }
