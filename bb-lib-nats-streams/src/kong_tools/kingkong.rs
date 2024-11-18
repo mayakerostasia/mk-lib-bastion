e14b0b6cbe0bba5ea5feef2ac038d018c4b77b90
nwisemanII <nwiseman@bluebastion.net>
2024-11-18 14:36:23 -0500

    fix error

--- bb-lib-nats-streams/src/kong_tools/kingkong.rs
+++ bb-lib-nats-streams/src/kong_tools/kingkong.rs
@@ -218,7 +218,7 @@ impl KingKong {
                 Err(err) => Err(anyhow!("Unable to listen for shutdown signal: {}", err)),
             }
         };
-        let fut2 = async { self.http_listener.clone().listen(None).await };
+        let fut2 = async { self.http_listener.clone().listen(None, 200, false).await };
         let cancel_token = self.cancel_token.cancelled();
 
         let health_failure = async {
